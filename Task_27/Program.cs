add9f237eff7dc12cece687fb3b3664d991b5883
«George» <prior21@mail.ru>
2022-08-03 19:40:49 +0300

    added task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число для подсчета суммы цифр: "); //просим ввести число
+int num = Convert.ToInt32(Console.ReadLine()); //считываем эти числа
+
+int sum = GetDigitSum(num); //присваеваем 
+
+Console.WriteLine($"{num} -> {sum}"); //выводим на экран
+
+
+int GetDigitSum(int number)
+{
+    int result = 0; 
+
+    while (number >= 1) //используем цикл
+    {
+        result += number % 10;
+        number /= 10;
+    }
+
+    return result; //возвращаем результат
+}

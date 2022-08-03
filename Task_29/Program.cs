67bba2f0e4f8d9a6025bb296e7d93616f4c95c2e
«George» <prior21@mail.ru>
2022-08-03 19:41:37 +0300

    added task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = GetRandomArray(8); //создаем массив  из 8 элементов
+WriteArrayToConsole(arr);
+
+int[] GetRandomArray(int arrayLength)
+{
+    Random rnd = new Random(); //используем метод рандом и перебираем его
+    int[] arr = new int[arrayLength];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next();
+    }
+    return arr;
+}
+
+void WriteArrayToConsole(int[] array) // используем метод который ничего не возвращает
+{
+    for (int i = 0; i < array.Length; i++) //
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(",");
+    }
+
+    Console.Write(" -> ["); //отображаем открытую скобку
+    for (int i = 0; i < array.Length; i++) 
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]"); // отображаем закрытую скобку
+}
